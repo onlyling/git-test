6963c39bcb033c5605ff133e02ebbca7c5c28890
LING_ZI_QING <onlylingstudio@gmail.com>
2021-02-13 19:03:26 +0800

    chore(lib): index
    测试

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-const text = '测试文案';
+let text = '测试文案';
 
 console.log(text);
 
-const text1 = 'ccc';
+let text1 = 'ccc';
 
 console.log(text1);
 
-const text2 = 'ccc';
+let text2 = 'ccc';
 
 console.log(text2);
