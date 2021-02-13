eb7563ef213f36cce109ce0b25b7dc71def7cb76
LING_ZI_QING <onlylingstudio@gmail.com>
2021-02-13 19:09:26 +0800

    feat(lib): index
    test eslint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-let text = '测试文案';
+const text = '测试文案';
 
 console.log(text);
 
-let text1 = 'ccc';
+const text1 = 'ccc';
 
 console.log(text1);
 
-let text2 = 'ccc';
+const text2 = 'ccc';
 
 console.log(text2);
